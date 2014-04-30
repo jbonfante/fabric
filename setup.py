4d472a7927a5df266db6af063d28aad545e6dd40
Juan Bonfante <juan.bonfante@gmail.com>
2014-04-29 20:36:25 -0500

    Frabric Py3k egg name
    Module [fabric-py3k]:
      * setup.py

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
           'Programming Language :: Python :: 2.5',
           'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
           'Topic :: Software Development',
           'Topic :: Software Development :: Build Tools',
           'Topic :: Software Development :: Libraries',
